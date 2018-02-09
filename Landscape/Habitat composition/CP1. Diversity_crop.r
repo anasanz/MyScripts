57b250b6097105f828190c0741452158fd8b5a6b
anasanz <anasanz.asp@gmail.com>
2018-02-09 15:21:04 +0100

    crop diversity

--- Landscape/Habitat composition/CP1. Diversity_crop.r
+++ Landscape/Habitat composition/CP1. Diversity_crop.r
@@ -51,7 +51,7 @@ b15_500<-gBuffer(cf15,byid = TRUE, width = 500)
 c15_5<-crop(d15,b15_500,snap = "out")
 #writeOGR(c15_5,"C:/Users/ana.sanz/Documents/GIS Ana/DUN",layer = "clip_b15_5",driver="ESRI Shapefile")
 
-#Mirar usos para ver cómo reclasificar
+#Mirar usos para ver c?mo reclasificar
 library(dplyr)
 library(tidyr)
 c15_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b15_5") 
@@ -61,7 +61,7 @@ j<-c15_5@data[which(c15_5@data$cultiu %in% f$cultiu), c(6,10)] #
 t<-j[which(!duplicated(j)),]
 k<-left_join(f,t)
 w<-spread(k,seca_regad,Freq)
-#write.csv(w,file = "usos_2015") #Reclasificación en excel Usos_buffer_dun_dg
+#write.csv(w,file = "usos_2015") #Reclasificaci?n en excel Usos_buffer_dun_dg
 
 
 #VARIABLE % BARBECHO
@@ -82,7 +82,7 @@ fallow<-raster(fal,layer=1)
 #writeRaster(fallow,filename='fallow_15',format='GTiff') #Layer with only % fallow in buffers 2015
 
 
-#VARIABLE % CULTIVOS LEÑOSOS
+#VARIABLE % CULTIVOS LE?OSOS
 #1. Reclassify
 c15_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b15_5")
 c15_5$tree<-as.character(c15_5$cultiu)
@@ -115,7 +115,7 @@ tree<-raster(tre,layer=2)
 #writeRaster(tree,filename='tree_15',format='GTiff') #Layer with only % tree in buffers 2015
 
 
-#VARIABLE % CULTIVOS EN REGADÍO INTENSIVO
+#VARIABLE % CULTIVOS EN REGAD?O INTENSIVO
 #1. Reclassify
 c15_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b15_5")
 c15_5$irri<-as.character(c15_5$cultiu)
@@ -145,6 +145,54 @@ ir<-layerize(ras15_irri,classes=NULL,bylayer=TRUE,suffix='numbers')
 irri<-raster(ir,layer=1)
 writeRaster(irri,filename='irri_15',format='GTiff') #Layer with only % irri in buffers 2015
 
+#VARIABLE CROP DIVERSITY
+#1. Reclassify
+c15_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b15_5")
+c15_5$cropdiv<-as.character(c15_5$grup)
+c15_5$cropdiv[c15_5$cultiu == "AMETLLERS"]<-"AMETLLERS"
+c15_5$cropdiv[c15_5$cultiu == "ALFALS"]<-"ALFALS"
+c15_5$cropdiv[c15_5$cultiu == "COLZA"]<-"COLZA"
+c15_5$cropdiv[c15_5$cultiu == "PESOLS"]<-"PESOLS"
+c15_5$cropdiv[c15_5$cultiu == "VECA I CIVADA"]<-"CEREALS"
+c15_5$cropdiv[c15_5$cropdiv == "LLEGUMINOSES"]<-"ALTRES"
+c15_5$cropdiv[c15_5$cropdiv == "HORTICOLES"]<-"ALTRES"
+c15_5$cropdiv[c15_5$cropdiv == "FARRATGERES"]<-"ALTRES"
+c15_5$cropdiv[c15_5$cultiu == "NOGUERES"]<-"ALTRES"
+c15_5$cropdiv[c15_5$cultiu == "CAMELINA"]<-"ALTRES"
+c15_5$cropdiv[c15_5$cultiu == "VIVER ARBRE I ARBUST"]<-"ALTRES"
+c15_5$cropdiv[c15_5$cultiu == "MILL"]<-"ALTRES"
+
+
+c15_5$cropdiv<-as.factor(c15_5$cropdiv) #Factor because otherwise rasterize doesnt work
+#2. Rasterize
+r1 <- raster(resolution=50, extent(c15_5), crs = "+proj=utm +zone=31 +ellps=GRS80 +units=m +no_defs") #Empty raster
+ras15_cropdiv <- rasterize(c15_5,r1,c15_5$cropdiv) #Create raster of cropdiv % in buffer (LAYER 1)
+#writeRaster(ras15_cropdiv,filename='ras15_cropdiv',format='GTiff',overwrite = TRUE)
+#3. Layerize
+crop<-layerize(ras15_cropdiv,classes=NULL,bylayer=TRUE,suffix='numbers')
+c1<-raster(crop,layer=1)
+c2<-raster(crop,layer=2)
+c3<-raster(crop,layer=3)
+c4<-raster(crop,layer=4)
+c5<-raster(crop,layer=5)
+c6<-raster(crop,layer=6)
+c7<-raster(crop,layer=7)
+c8<-raster(crop,layer=8)
+c9<-raster(crop,layer=9)
+c10<-raster(crop,layer=10)
+c11<-raster(crop,layer=11)
+
+writeRaster(c1,filename='c1_15',format='GTiff') #Layer with only % of each land category 
+writeRaster(c2,filename='c2_15',format='GTiff') #in buffers 2015
+writeRaster(c3,filename='c3_15',format='GTiff')
+writeRaster(c4,filename='c4_15',format='GTiff')
+writeRaster(c5,filename='c5_15',format='GTiff')
+writeRaster(c6,filename='c6_15',format='GTiff')
+writeRaster(c7,filename='c7_15',format='GTiff')
+writeRaster(c8,filename='c8_15',format='GTiff')
+writeRaster(c9,filename='c9_15',format='GTiff')
+writeRaster(c10,filename='c10_15',format='GTiff')
+writeRaster(c11,filename='c11_15',format='GTiff')
 
 #2016 !!!!!!!!!!!!!!!!!!!!!!
 
@@ -156,7 +204,7 @@ b16_500<-gBuffer(cf16,byid = TRUE, width = 500)
 c16_5<-crop(d16,b16_500,snap = "out")
 #writeOGR(c16_5,"C:/Users/ana.sanz/Documents/GIS Ana/DUN",layer = "clip_b16_5",driver="ESRI Shapefile")
 
-#Mirar usos para ver cómo reclasificar
+#Mirar usos para ver c?mo reclasificar
 c16_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b16_5") 
 f<-as.data.frame(xtabs(~ cultiu + seca_regad,c16_5@data)) #2015, frecuencia usos
 colnames(f)[1]<-"cultiu" #mismo nombre para luego unir
@@ -164,7 +212,7 @@ j<-c16_5@data[which(c16_5@data$cultiu %in% f$cultiu), c(2,7)] #
 t<-j[which(!duplicated(j)),]
 k<-left_join(f,t)
 w<-spread(k,seca_regad,Freq)
-#write.csv(w,file = "usos_2016") #Reclasificación en excel Usos_buffer_dun_dg
+#write.csv(w,file = "usos_2016") #Reclasificaci?n en excel Usos_buffer_dun_dg
 
 #VARIABLE % BARBECHO
 #1. Reclassify
@@ -182,7 +230,7 @@ fal<-layerize(ras16_fallow,classes=NULL,bylayer=TRUE,suffix='numbers')
 fallow<-raster(fal,layer=1)
 writeRaster(fallow,filename='fallow_16',format='GTiff') #Layer with only % fallow in buffers 2016
 
-#VARIABLE % CULTIVOS LEÑOSOS
+#VARIABLE % CULTIVOS LE?OSOS
 #1. Reclassify
 c16_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b16_5")
 c16_5$tree<-as.character(c16_5$cultiu)
@@ -220,7 +268,7 @@ tree<-raster(tre,layer=2)
 #writeRaster(tree,filename='tree_16',format='GTiff') #Layer with only % tree in buffers 2016
 
 
-#VARIABLE % CULTIVOS EN REGADÍO INTENSIVO
+#VARIABLE % CULTIVOS EN REGAD?O INTENSIVO
 #1. Reclassify
 c16_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b16_5")
 c16_5$irri<-as.character(c16_5$cultiu)
@@ -241,10 +289,10 @@ c16_5$irri[c16_5$irri == "KIWI"]<-"irri"
 c16_5$irri[c16_5$irri == "PRUNERES"]<-"irri"
 c16_5$irri[c16_5$irri == "CARABASSA"]<-"irri"
 c16_5$irri[c16_5$irri == "ALTRES FRUITERS"]<-"irri"
-c16_5$irri[c16_5$irri == "CEBES, CALÇOTS, PORROS I ALLS"]<-"irri"
+c16_5$irri[c16_5$irri == "CEBES, CAL?OTS, PORROS I ALLS"]<-"irri"
 c16_5$irri[c16_5$irri == "FIGUERA"]<-"irri"
 c16_5$irri[c16_5$irri == "MAGRANER"]<-"irri"
-c16_5$irri[c16_5$irri == "MELÓ"]<-"irri"
+c16_5$irri[c16_5$irri == "MEL?"]<-"irri"
 c16_5$irri[c16_5$irri != "irri"]<-"others"
 c16_5$irri<-as.factor(c16_5$irri) #Factor because otherwise rasterize doesnt work
 #2. Rasterize
@@ -256,6 +304,46 @@ ir<-layerize(ras16_irri,classes=NULL,bylayer=TRUE,suffix='numbers')
 irri<-raster(ir,layer=1)
 #writeRaster(irri,filename='irri_16',format='GTiff') #Layer with only % irri in buffers 2016
 
+#VARIABLE CROP DIVERSITY
+#1. Reclassify
+c16_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b16_5")
+c16_5$cropdiv<-as.character(c16_5$grup)
+c16_5$cropdiv[c16_5$cultiu == "AMETLLERS"]<-"AMETLLERS"
+c16_5$cropdiv[c16_5$cropdiv == "CEREALS D'ESTIU"]<-"BLAT DE MORO"
+c16_5$cropdiv[c16_5$cultiu == "ALFALS"]<-"ALFALS"
+c16_5$cropdiv[c16_5$cropdiv == "PROTEAGINOSES"]<-"PÃˆSOLS"
+c16_5$cropdiv[c16_5$cultiu == "VE?A I CIVADA"]<-"CEREALS"
+c16_5$cropdiv[c16_5$cropdiv == "LLEGUMINOSES"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cropdiv == "HORT?COLES"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cultiu == "COLZA"]<-"COLZA"
+c16_5$cropdiv[c16_5$cultiu == "FESTUCA"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cultiu == "TREPADELLA"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cultiu == "NOGUERES"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cropdiv == "ALTRES PRODUCTES"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cultiu == "GIRA-SOL"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cultiu == "RAY-GRASS"]<-"ALTRES"
+c16_5$cropdiv[c16_5$cultiu == "AVELLANER"]<-"AMETLLERS"
+c16_5$cropdiv[c16_5$cultiu == "PISTATXER O FESTUC"]<-"AMETLLERS"
+
+
+c16_5$cropdiv<-as.factor(c16_5$cropdiv) #Factor because otherwise rasterize doesnt work
+#2. Rasterize
+r1 <- raster(resolution=50, extent(c16_5), crs = "+proj=utm +zone=31 +ellps=GRS80 +units=m +no_defs") #Empty raster
+ras16_cropdiv <- rasterize(c16_5,r1,c16_5$cropdiv) #Create raster of cropdiv % in buffer (LAYER 1)
+#writeRaster(ras16_cropdiv,filename='ras16_cropdiv',format='GTiff',overwrite = TRUE)
+#3. Layerize
+crop<-layerize(ras16_cropdiv,classes=NULL,bylayer=TRUE,suffix='numbers')
+c1<-raster(crop,layer=1)
+c2<-raster(crop,layer=2)
+c3<-raster(crop,layer=3)
+c4<-raster(crop,layer=4)
+c5<-raster(crop,layer=5)
+c6<-raster(crop,layer=6)
+c7<-raster(crop,layer=7)
+c8<-raster(crop,layer=8)
+c9<-raster(crop,layer=9)
+c10<-raster(crop,layer=10)
+c11<-raster(crop,layer=11)
 
 #2017 !!!!!!!!!!!!!!!!!!!!!!
 
@@ -267,7 +355,7 @@ b17_500<-gBuffer(cf17,byid = TRUE, width = 500)
 c17_5<-crop(d17,b17_500,snap = "out")
 #writeOGR(c17_5,"C:/Users/ana.sanz/Documents/GIS Ana/DUN",layer = "clip_b17_5",driver="ESRI Shapefile")
 
-#Mirar usos para ver cómo reclasificar
+#Mirar usos para ver c?mo reclasificar
 c17_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b17_5") 
 f<-as.data.frame(xtabs(~ cultiu + seca_regad,c17_5@data)) #2015, frecuencia usos
 colnames(f)[1]<-"cultiu" #mismo nombre para luego unir
@@ -275,7 +363,7 @@ j<-c17_5@data[which(c17_5@data$cultiu %in% f$cultiu), c(4,9)] #
 t<-j[which(!duplicated(j)),]
 k<-left_join(f,t)
 w<-spread(k,seca_regad,Freq)
-write.csv(w,file = "usos_2017.csv") #Reclasificación en excel Usos_buffer_dun_dg
+write.csv(w,file = "usos_2017.csv") #Reclasificaci?n en excel Usos_buffer_dun_dg
 
 #VARIABLE % BARBECHO
 #1. Reclassify
@@ -293,7 +381,7 @@ fal<-layerize(ras17_fallow,classes=NULL,bylayer=TRUE,suffix='numbers')
 fallow<-raster(fal,layer=1)
 writeRaster(fallow,filename='fallow_17',format='GTiff') #Layer with only % fallow in buffers 2017
 
-#VARIABLE % CULTIVOS LEÑOSOS
+#VARIABLE % CULTIVOS LE?OSOS
 #1. Reclassify
 c17_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b17_5")
 c17_5$tree<-as.character(c17_5$cultiu)
@@ -330,7 +418,7 @@ tree<-raster(tre,layer=2)
 #writeRaster(tree,filename='tree_17',format='GTiff') #Layer with only % tree in buffers 2017
 
 
-#VARIABLE % CULTIVOS EN REGADÍO INTENSIVO
+#VARIABLE % CULTIVOS EN REGAD?O INTENSIVO
 #1. Reclassify
 c17_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b17_5")
 c17_5$irri<-as.character(c17_5$cultiu)
@@ -366,6 +454,47 @@ irri<-raster(ir,layer=1)
 #writeRaster(irri,filename='irri_17',format='GTiff') #Layer with only % irri in buffers 2016
 
 
+#VARIABLE CROP DIVERSITY
+#1. Reclassify
+c17_5<-readOGR("C:/Users/ana.sanz/Documents/GIS Ana/DUN", "clip_b17_5")
+c17_5$cropdiv<-as.character(c17_5$grup)
+c17_5$cropdiv[c17_5$cultiu == "ALFALS"]<-"ALFALS"
+c17_5$cropdiv[c17_5$cultiu == "AMETLLERS"]<-"AMETLLERS"
+c17_5$cropdiv[c17_5$cultiu == "AVELLANER"]<-"AMETLLERS"
+c17_5$cropdiv[c17_5$cropdiv == "CEREALS D'ESTIU"]<-"BLAT DE MORO"
+c17_5$cropdiv[c17_5$cropdiv == "HORTICOLES"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "CAMELINA"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "COLZA"]<-"COLZA"
+c17_5$cropdiv[c17_5$cropdiv == "ALTRES PRODUCTES"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "FESTUCA"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "GIRA-SOL"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "NOGUERES"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "RAY-GRASS"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "TREPADELLA"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "PESOLS"]<-"PESOLS"
+c17_5$cropdiv[c17_5$cropdiv == "LLEGUMINOSES"]<-"ALTRES"
+c17_5$cropdiv[c17_5$cultiu == "VERA I CIVADA"]<-"CEREALS"
+
+
+c17_5$cropdiv<-as.factor(c17_5$cropdiv) #Factor because otherwise rasterize doesnt work
+#2. Rasterize
+r1 <- raster(resolution=50, extent(c17_5), crs = "+proj=utm +zone=31 +ellps=GRS80 +units=m +no_defs") #Empty raster
+ras17_cropdiv <- rasterize(c17_5,r1,c17_5$cropdiv) #Create raster of cropdiv % in buffer (LAYER 1)
+#writeRaster(ras17_cropdiv,filename='ras17_cropdiv',format='GTiff',overwrite = TRUE)
+#3. Layerize
+crop<-layerize(ras17_cropdiv,classes=NULL,bylayer=TRUE,suffix='numbers')
+c1<-raster(crop,layer=1)
+c2<-raster(crop,layer=2)
+c3<-raster(crop,layer=3)
+c4<-raster(crop,layer=4)
+c5<-raster(crop,layer=5)
+c6<-raster(crop,layer=6)
+c7<-raster(crop,layer=7)
+c8<-raster(crop,layer=8)
+c9<-raster(crop,layer=9)
+c10<-raster(crop,layer=10)
+c11<-raster(crop,layer=11)
+
 ###############################################################################################
 #All variables created, now extract values from variables with the different buffer sizes
 
@@ -385,15 +514,21 @@ irri15<-raster('irri_15.tif')
 irri16<-raster('irri_16.tif') 
 irri17<-raster('irri_17.tif')
 
-stack15<-stack(fallow15,tree15,irri15,RAT=TRUE) # One stack per year because the extents are different
-stack16<-stack(fallow16,tree16,irri16,RAT=TRUE)
-stack17<-stack(fallow17,tree17,irri17,RAT=TRUE)
+# Stack including crop diversity rasters calculated before
+
+stack15<-stack(fallow15, tree15, irri15,
+               c1, c2, c3, c4, c5, c6, c7, 
+               c8, c9, c10, c11, RAT=TRUE) # One stack per year because the extents are different
+stack16<-stack(fallow16,tree16,irri16,
+               c1, c2, c3, c4, c5, c6, c7, 
+               c8, c9, c10, c11, RAT=TRUE)
+stack17<-stack(fallow17,tree17,irri17,
+               c1, c2, c3, c4, c5, c6, c7, 
+               c8, c9, c10, c11, RAT=TRUE)
 
 #2. EXTRACT
 library(dplyr)
 setwd("C:/Users/ana.sanz/Documents/Datos/Datos barbechos arrendados/Variables")
-
-
 
 #2015
 fin15<-as.data.frame(fin15@data[,c(colnames(fin15@data) %in% c("ID","Codi_Finca"))])
@@ -440,3 +575,39 @@ b17_500<-as(b17_500,"SpatialPolygonsDataFrame")
 comp17_500<-extract(stack17,b17_500,fun = mean,na.rm = TRUE,df = TRUE,factors = TRUE,sp = TRUE)
 comp17_500<-bind_cols (fin17,as.data.frame(comp17_500))
 #write.csv(comp17_500,"comp17_500.csv")
+
+#############################################################
+#SHANNON DIVERSITY INDEX 
+
+#2015
+library(vegan)
+shan_15_200<-as.data.frame(diversity(comp15_200[ ,c(8:18)],index = "shannon"))
+colnames(shan_15_200)[1] <- "shan_15_200"
+shan_15_500<-as.data.frame(diversity(comp15_500[ ,c(8:18)],index = "shannon"))
+colnames(shan_15_500)[1] <- "shan_15_500"
+
+crop_div_15 <- data.frame(shan_15_200,shan_15_500)
+#write.csv(crop_div_15,"crop_div_15.csv")
+
+shan_16_200<-as.data.frame(diversity(comp16_200[ ,c(8:18)],index = "shannon"))
+colnames(shan_16_200)[1] <- "shan_16_200"
+shan_16_500<-as.data.frame(diversity(comp16_500[ ,c(8:18)],index = "shannon"))
+colnames(shan_16_500)[1] <- "shan_16_500"
+
+crop_div_16 <- data.frame(shan_16_200,shan_16_500)
+#write.csv(crop_div_16,"crop_div_16.csv")
+
+shan_17_200<-as.data.frame(diversity(comp17_200[ ,c(7:17)],index = "shannon"))
+colnames(shan_17_200)[1] <- "shan_17_200"
+shan_17_500<-as.data.frame(diversity(comp17_500[ ,c(7:17)],index = "shannon"))
+colnames(shan_17_500)[1] <- "shan_17_500"
+
+crop_div_17 <- data.frame(shan_17_200,shan_17_500)
+#write.csv(crop_div_17,"crop_div_17.csv")
+
+
+
+
+
+write.csv()
+
