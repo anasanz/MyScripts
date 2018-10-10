7897d3fbfa001f8b339b80f422419ded3e0c2865
anasanz <anasanz.asp@gmail.com>
2018-10-09 18:11:25 -0700

    AES 2010 and 2011

--- Ch. 2/AES.R
+++ Ch. 2/AES.R
@@ -6,7 +6,191 @@ library(dplyr)
 library(raster)
 library(tidyr)
 
-# ---- Before 2014 there is no AES
+# ---- 2010 ----
+# Load dun2010 (with measure 312)
+
+setwd("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/DUN/ID_REC_AES")
+
+d10 <- read.csv("IDREC_AES_2010.csv", sep = ";")
+
+# Limpiar columnas
+
+colnames(d10)[which(colnames(d10) == "Superfície.declarada")] <- "HA_DEC"
+colnames(d10)[which(colnames(d10) == "Superfície.SIGPAC")] <- "HA_SP"
+colnames(d10)[which(colnames(d10) == "Ús.SIGPAC")] <- "US_SP"
+colnames(d10)[which(colnames(d10) == "Nom.producte")] <- "PROD_NOM"
+colnames(d10)[which(colnames(d10) == "Campanya")] <- "CAMPANYA"
+
+
+d10 <- d10[, which(colnames(d10) %in% c("CAMPANYA", "ID_REC", "HA_DEC", "HA_SP", 
+                                                       "US_SP", "PROD_NOM"))]
+
+d10$HA_Crop <- 0
+d10$STRIP <- 0
+d10$HA_Fallow <- 0
+d10$id_obj <- seq(1,nrow(d10),1)
+
+d10$PROD_NOM <- as.character(d10$PROD_NOM) #To simplify: Any crop = CROP and any fallow = FALLOW
+unique(d10$PROD_NOM)
+d10$PROD_NOM[which(d10$PROD_NOM %in% c("ORDI", "BLAT TOU", "ALTRES CEREALS", "TRITICALE", "CIVADA"))] <- "CROP"
+d10$PROD_NOM[which(d10$PROD_NOM %in% c("SUPERF-CIE LLIURE DE SEMBRA", "RETIRADA VOLUNT+RIA","GUARETS TRADICIONALS", "GUARETS MEDIAMBIENTAL COMPLEME" ))] <- "FALLOW"
+
+#Make easier the join later
+d10$CAMPANYA <- as.character(d10$CAMPANYA) 
+d10$ID_REC <- as.character(d10$ID_REC) 
+d10$US_SP <- as.character(d10$US_SP)
+d10$HA_DEC <- as.numeric(d10$HA_DEC)
+d10$id_obj <- as.character(d10$id_obj)
+
+#Check duplicated
+
+dup_all <- d10[which(duplicated(d10$ID_REC)), ] #Identify duplicates (franjas)
+dup_all$ID_REC <- as.character(dup_all$ID_REC)
+id_dup <- unique(dup_all$ID_REC) # List ids duplicated
+dup <- d10[which(d10$ID_REC %in% id_dup), ] # Only to check
+dup <- arrange(dup,desc(ID_REC))
+
+#Data frame to store
+
+df <- as.data.frame(matrix(0, nrow = length(id_dup), ncol = 8))
+colnames(df) <- c("CAMPANYA", "ID_REC", "HA_SP", 
+                  "US_SP","HA_Crop", "STRIP","HA_Fallow","id_obj")
+
+#Make easier the join later
+df$CAMPANYA <- as.character(df$CAMPANYA) 
+df$ID_REC <- as.character(df$ID_REC) 
+df$US_SP <- as.character(df$US_SP)
+df$id_obj <- as.character(df$id_obj)
+
+
+# Combine information strips
+
+for (i in 1:length(id_dup)){
+  
+  tmp <- d10[which(d10$ID_REC == id_dup[i]), ] # Select the duplicated rows for each franja
+  tmp_spread <- spread(tmp, PROD_NOM, HA_DEC, fill = NA, drop = TRUE) # Spread to isolate the size of crop/fallows
+  
+  tmp_spread$HA_Crop <- sum(tmp_spread$CROP[which(!is.na(tmp_spread$CROP))]) # Sum in case there is more than one crop and place
+  tmp_spread$HA_Fallow <- sum(tmp_spread$FALLOW[which(!is.na(tmp_spread$FALLOW))]) # number in unique variable for field HA_crop/fallow
+  
+  tmp_unique <- tmp_spread[-which(duplicated(tmp_spread$ID_REC)), 
+                           -which(colnames(tmp_spread) %in% c("CROP", "FALLOW"))] #Remove duplicates and crop/fallow cols
+  df[i, ] <- tmp_unique
+}
+
+# Identify strips (franjas): 1/0
+for (i in 1:nrow(df)){
+  if (df$HA_Crop[i] > 0 & df$HA_Fallow[i] > 0) {df$STRIP[i] <- 1}
+}
+
+# Join select id_rec from sigpac that are AES (id from df)
+# Load sigpac2010
+sp10 <- readOGR("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/SIGPAC_2010", layer = "sp10_clip")
+
+# Same name df = sigpac
+colnames(df)[colnames(df) == "ID_REC"] <- "CODI_REC"
+id_aes <- unique(df$CODI_REC) # Take IDs from 
+
+sp10_aes <- sp10[which(sp10@data$CODI_REC %in% id_aes), ] #clip only aes
+head(sp10_aes@data)
+# Join data from df (size of fields and strips from dun)
+sp10_aes@data <- sp10_aes@data[ ,which(colnames(sp10_aes@data) %in% c("CODI_REC", "SUP_M2", "SUP_HA", 
+                                                                      "PENDENT", "Shape_Leng", "Shape_Area"))]
+sp10_aes@data <-left_join(sp10_aes@data, df, by = "CODI_REC")
+
+#setwd("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/Only AES")
+#writeOGR(sp10_aes,
+#         dsn = "C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/Only AES", layer = "AES_2010", driver = "ESRI Shapefile")
+
+# ---- 2011 ----
+# Load dun2011 (with measure 312)
+
+setwd("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/DUN/ID_REC_AES")
+
+d11 <- read.csv("IDREC_AES_2011.csv", sep = ";")
+
+# Limpiar columnas
+
+colnames(d11)[which(colnames(d11) == "SuperfComprovada")] <- "HA_DEC"
+colnames(d11)[which(colnames(d11) =="RecinteSIGPAC")] <- "ID_REC"
+colnames(d11)[which(colnames(d11) == "Descripció.ajut")] <- "PROD_NOM"
+colnames(d11)[which(colnames(d11) == "Campanya")] <- "CAMPANYA"
+
+
+d11 <- d11[, which(colnames(d11) %in% c("CAMPANYA", "ID_REC", "HA_DEC", "PROD_NOM"))]
+
+d11$HA_Crop <- 0
+d11$STRIP <- 0
+d11$HA_Fallow <- 0
+d11$id_obj <- seq(1,nrow(d11),1)
+
+d11$PROD_NOM <- as.character(d11$PROD_NOM) #To simplify: Any crop = CROP and any fallow = FALLOW
+unique(d11$PROD_NOM)
+d11$PROD_NOM[which(d11$PROD_NOM %in% "ZEPA: cereal hivern")] <- "CROP"
+d11$PROD_NOM[which(d11$PROD_NOM %in% "ZEPAs:guaret mediamb")] <- "FALLOW"
+
+#Make easier the join later
+d11$CAMPANYA <- as.character(d11$CAMPANYA) 
+d11$ID_REC <- as.character(d11$ID_REC) 
+d11$HA_DEC <- as.numeric(d11$HA_DEC)
+d11$id_obj <- as.character(d11$id_obj)
+
+#Check duplicated
+
+dup_all <- d11[which(duplicated(d11$ID_REC)), ] #Identify duplicates (franjas)
+dup_all$ID_REC <- as.character(dup_all$ID_REC)
+id_dup <- unique(dup_all$ID_REC) # List ids duplicated
+dup <- d11[which(d11$ID_REC %in% id_dup), ] # Only to check
+dup <- arrange(dup,desc(ID_REC))
+
+#Data frame to store
+
+df <- as.data.frame(matrix(0, nrow = length(id_dup), ncol = 6))
+colnames(df) <- c("CAMPANYA", "ID_REC","HA_Crop", "STRIP","HA_Fallow","id_obj")
+
+#Make easier the join later
+df$CAMPANYA <- as.character(df$CAMPANYA) 
+df$ID_REC <- as.character(df$ID_REC) 
+df$id_obj <- as.character(df$id_obj)
+
+# Combine information strips
+
+for (i in 1:length(id_dup)){
+  
+  tmp <- d11[which(d11$ID_REC == id_dup[i]), ] # Select the duplicated rows for each franja
+  tmp_spread <- spread(tmp, PROD_NOM, HA_DEC, fill = NA, drop = TRUE) # Spread to isolate the size of crop/fallows
+  
+  tmp_spread$HA_Crop <- sum(tmp_spread$CROP[which(!is.na(tmp_spread$CROP))]) # Sum in case there is more than one crop and place
+  tmp_spread$HA_Fallow <- sum(tmp_spread$FALLOW[which(!is.na(tmp_spread$FALLOW))]) # number in unique variable for field HA_crop/fallow
+  
+  tmp_unique <- tmp_spread[-which(duplicated(tmp_spread$ID_REC)), 
+                           -which(colnames(tmp_spread) %in% c("CROP", "FALLOW"))] #Remove duplicates and crop/fallow cols
+  df[i, ] <- tmp_unique
+}
+
+# Identify strips (franjas): 1/0
+for (i in 1:nrow(df)){
+  if (df$HA_Crop[i] > 0 & df$HA_Fallow[i] > 0) {df$STRIP[i] <- 1}
+}
+
+# Join select id_rec from sigpac that are AES (id from df)
+# Load sigpac2011
+sp11 <- readOGR("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/SIGPAC_2011", layer = "SP11_REC_clip")
+
+id_aes <- unique(df$ID_REC) # Take IDs from aes (dun)
+
+sp11_aes <- sp11[which(sp11@data$ID_REC %in% id_aes), ] #clip only aes
+head(sp11_aes@data)
+
+# Join data from df (size of fields and strips from dun)
+sp11_aes@data <- sp11_aes@data[ ,which(colnames(sp11_aes@data) %in% c("ID_REC", "HA","PENDENT"))]
+sp11_aes@data <-left_join(sp11_aes@data, df, by = "ID_REC")
+
+setwd("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/Only AES")
+writeOGR(sp11_aes,
+         dsn = "C:/OneDrive/PhD/Second chapter/Data/GIS/AES/2010-2014/Only AES", layer = "AES_2011", driver = "ESRI Shapefile")
+
+
 # ---- 2014 ----
 
 #Load data
@@ -225,7 +409,112 @@ writeOGR(d15_full2,dsn = "C:/OneDrive/PhD/Second chapter/Data/GIS/AES/Codi364_DU
 
 
 
-# ---- 2016 (esperar mónica) ----
+# ---- 2016 ----
+
+# Upload the projected dun layer
+#d16 <- readOGR("C:/Users/ana.sanz/OneDrive/PhD/Second chapter/Data/GIS/AES/Codi364_DUN2015-2017/364/364/364_2015.shp")
+d16 <- readOGR("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/Codi364_DUN2016-2017/Only AES/364", layer = "364_2016_proj50")
+
+head(d16@data)
+
+# Strip digitalized! So decide what Im gonna do as for 2017...
+
+
+# Limpiar columnas
+
+d16@data <- d16@data[, which(colnames(d16@data) %in% c("OBJECTID", "CAMPANYA", "ID_REC", "M2_SP", "HA_SP", "HA_DEC", 
+                                                       "US_SP", "PENDENT", "PROD_NUM", "PROD_NOM", "AJUTS", "ZEPA"))]
+
+d16@data$HA_Crop <- 0
+d16@data$STRIP <- 0
+d16@data$HA_Fallow <- 0
+
+d16@data$PROD_NOM <- as.character(d16@data$PROD_NOM) #To simplify: Any crop = CROP and any fallow = FALLOW
+unique(d16@data$PROD_NOM)
+d16@data$PROD_NOM[which(d16@data$PROD_NOM %in% c("ORDI", "BLAT TOU", "SÃˆGOL", "TRITICALE", "CIVADA"))] <- "CROP"
+d16@data$PROD_NOM[which(d16@data$PROD_NOM %in% c("GUARET SIE/ SUP. LLIURE SEMBRA", "GUARET NO SIE/ SUP. LLIURE SE*"))] <- "FALLOW"
+
+d16@data$AJUTS <- as.character(d16@data$AJUTS) #Make easier the join later
+d16@data$CAMPANYA <- as.character(d16@data$CAMPANYA) 
+d16@data$ID_REC <- as.character(d16@data$ID_REC) 
+d16@data$US_SP <- as.character(d16@data$US_SP) 
+
+#Check duplicated
+
+dup_all <- d16@data[which(duplicated(d16@data$ID_REC)), ] #Identify duplicates (franjas)
+dup_all$ID_REC <- as.character(dup_all$ID_REC)
+id_dup <- unique(dup_all$ID_REC) # List ids duplicated
+dup <- d16@data[which(d16@data$ID_REC %in% id_dup), ] # Only to check
+
+#Data frame to store
+
+df <- as.data.frame(matrix(0, nrow = length(id_dup), ncol = 13))
+colnames(df) <- c("OBJECTID", "CAMPANYA","ID_REC","M2_SP","HA_SP","US_SP",
+                  "PENDENT","PROD_NUM","AJUTS","ZEPA","HA_Crop","STRIP","HA_Fallow")
+
+df$AJUTS <- as.character(df$AJUTS) #Make easier the join later
+df$CAMPANYA <- as.character(df$CAMPANYA) 
+df$ID_REC <- as.character(df$ID_REC) 
+df$US_SP <- as.character(df$US_SP) 
+
+# Combine information strips
+
+for (i in 1:length(id_dup)){
+  
+  tmp <- d16@data[which(d16@data$ID_REC == id_dup[i]), ] # Select the duplicated rows for each franja
+  tmp_spread <- spread(tmp, PROD_NOM, HA_DEC, fill = NA, drop = TRUE) # Spread to isolate the size of crop/fallows
+  
+  tmp_spread$HA_Crop <- sum(tmp_spread$CROP[which(!is.na(tmp_spread$CROP))]) # Sum in case there is more than one crop and place
+  tmp_spread$HA_Fallow <- sum(tmp_spread$FALLOW[which(!is.na(tmp_spread$FALLOW))]) # number in unique variable for field HA_crop/fallow
+  
+  tmp_unique <- tmp_spread[-which(duplicated(tmp_spread$ID_REC)), 
+                           -which(colnames(tmp_spread) %in% c("CROP", "FALLOW"))] #Remove duplicates and crop/fallow cols
+  
+  df[i, ] <- tmp_unique
+}
+
+# Identify strips (franjas): 1/0
+for (i in 1:nrow(df)){
+  if (df$HA_Crop[i] > 0 & df$HA_Fallow[i] > 0) {df$STRIP[i] <- 1}
+}
+
+# Make df having the same columns (add prod_nom and ha_dec).
+df$PROD_NOM <- "DUP"
+df$HA_DEC <- NA
+
+# df joins information from strips and the ones that are duplicated and are not strips (eg: two different crops in same rec)
+
+
+# Join with spatial information
+
+#1. Join information to layer with only duplicated IDs
+
+d16_dup <- d16[which(d16@data$ID_REC %in% id_dup), ] # Layer with only ids that are duplicated
+d16_dup@data$ID_REC <- as.character(d16_dup@data$ID_REC)
+d16_dup@data <- d16_dup@data[ ,c(2,3)] # Keep only the ID_REC, OBJ_ID y CAMPANYA (Año)
+d16_dup <- d16_dup[-which(duplicated(d16_dup@data$ID_REC)), ] #Remove duplicated
+
+# Join df - d16_dup
+d16_dup@data <- left_join(d16_dup@data, df, by = "ID_REC") # Spatial features duplicated
+
+#Fix Campanya name to make it fit
+d16_dup@data <- d16_dup@data[ ,-4]
+colnames(d16_dup@data)[1] <- "CAMPANYA"
+
+# 2. Merge d16_dup with the rest of the layer without duplicates
+d16_nodup <- d16[-which(d16@data$ID_REC %in% id_dup), ] # Layer with only ids that are NOT duplicated
+head(d16_nodup@data)
+head(d16_dup@data) # Same names. Ready to merge
+
+
+
+#d16_full <- union(d16_dup,d16_nodup)
+d16_full2 <- bind(d16_dup,d16_nodup)
+
+
+setwd("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/Codi364_DUN2016-2017/Only AES/364")
+writeOGR(d16_full2,dsn = "C:/OneDrive/PhD/Second chapter/Data/GIS/AES/Codi364_DUN2016-2017/Only AES/364", layer = "AES_2016_proj", driver = "ESRI Shapefile")
+
 # ---- 2017 ----
 d17 <- readOGR("C:/OneDrive/PhD/Second chapter/Data/GIS/AES/Codi364_DUN2015-2017/Only AES/364", layer = "364_2017_proj50")
 
