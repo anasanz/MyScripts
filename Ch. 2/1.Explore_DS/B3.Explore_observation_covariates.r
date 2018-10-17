941be7a93177c66efa618d128835e46ef88cb810
anasanz <anasanz.asp@gmail.com>
2018-10-17 13:11:42 -0700

    exploration

--- Ch. 2/1.Explore_DS/B3.Explore_observation_covariates.r
+++ Ch. 2/1.Explore_DS/B3.Explore_observation_covariates.r
@@ -1,57 +1,61 @@
 
 rm(list=ls())
 
-library(rgdal)
-library(dplyr)
-library(tidyr)
-library(sp)
-
+# Load
 setwd("C:/Users/Ana/Documents/PhD/Second chapter/Data")
-dat <- read.csv("DataDS_ready.csv")
+dat <- read.csv("DataDS_ready.csv") # All data
+setwd("C:/Users/Ana/Documents/PhD/Second chapter/Data/Explore_species_occurrence/All")
+num <- read.csv("NumberTrans_sp_year.csv") # Data to see most frequent species
+prop <- read.csv("PropTrans_sp_year.csv")
+# Most detected species are: 
+# - Target: Mecal, terax, buoed
+# - Other: Gacri, Mical, Pipic, Copal, Hirus, Padom
+target <- c("MECAL", "TERAX", "BUOED")
+other <- c("GACRI", "MICAL", "PIPIC", "COPAL", "HIRUS", "PADOM")
 
+# ---- Temperature ----
 
-# ---- All Species ----
+#  All Species 
 
-# All sp - Temperature
 dat_temp <- dat[-which(dat$Temp > 40),]
 hist(dat_temp$Temp, main = "All sp - Temperature") # 15ºC looks like the best
 
-# All sp - Distance with different winds
+# Target species 
 
+dat_temp <- dat[-which(dat$Temp > 40),]
+par(mfrow = c(1,3))
+for (i in 1:3){
+  hist(dat_temp$Temp[which(dat_temp$Species %in% target[i])], main = paste(target[i], "- Temperature")) 
+}
+
+# Other species
+
+dat_temp <- dat[-which(dat$Temp > 40),]
+par(mfrow = c(2,3))
+for (i in 1:length(other)){
+  hist(dat_temp$Temp[which(dat_temp$Species %in% other[i])], main = paste(other[i], "- Temperature")) 
+}
+
+
+# ---- Wind ----
+# Frequency - Distance with different winds
 xtabs(~Wind, dat)
+wind <- c(0:6)
 
-par(mfrow = c(3,3),
-    oma = c(3,3,4,1) + 0.1,
-    mar = c(2,2,2,0) + 0.1)
-
-w0 <- dat[which(dat$Wind == 0), ]
-hist(w0$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 0",
+# All species
+par(mfrow = c(3,3))
+for (i in 1:length(wind)){
+w <- dat[which(dat$Wind == wind[i]), ]
+hist(w$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = paste("All sp - Wind ",wind[i]),
      freq = FALSE)
+}
 
-w1 <- dat[which(dat$Wind == 1), ]
-hist(w1$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 1",
-     freq = FALSE) 
+#
 
-w2 <- dat[which(dat$Wind == 2), ]
-hist(w2$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 2",
-     freq = FALSE)
 
-w3 <- dat[which(dat$Wind == 3), ]
-hist(w3$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 3",
-     freq = FALSE)
 
-w4 <- dat[which(dat$Wind == 4), ]
-hist(w4$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 4",
-     freq = FALSE)
 
-w5 <- dat[which(dat$Wind == 5), ]
-hist(w5$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 5",
-     freq = FALSE)
 
-w6 <- dat[which(dat$Wind == 5), ]
-hist(w5$distance, breaks = c(0,25,50,99,200), xlab = "Distance bins (x)", col = "grey", main = "All sp - Wind 6",
-     freq = FALSE)
 
-# The number of detections slightly decreases for all distances except the 1sr bin
 
 
