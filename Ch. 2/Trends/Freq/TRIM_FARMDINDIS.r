bd64a2da1083657bb2506666ab07a9bcfa8c1220
anasanz <anasanz.asp@gmail.com>
2019-02-12 18:24:48 +0100

    trim

--- Ch. 2/Trends/Freq/TRIM_FARMDINDIS.r
+++ Ch. 2/Trends/Freq/TRIM_FARMDINDIS.r
@@ -1,5 +1,4 @@
 
-
 rm(list=ls())
 
 library(rtrim)
@@ -16,13 +15,61 @@ count_data <- rbind(ambSG, zepaSG, occSG, oriSG)
 sp <- unique(count_data$sp)
 ambit <- unique(count_data$id_ambit)
 
-m <- as.data.frame(matrix(NA, ncol = 13, nrow = 9)) # nrow es numero de a�os * numero de zonas en la que quiero estimar tendencias (para una especie)
-colnames(m) <- c("sp", "sector", "year", "index", "index_se", "IC95+", "IC95-", "coef_trend", "coef_trend_se", "ptrend", "pgof", "annual_trend", "description")
+m <- as.data.frame(matrix(NA, ncol = 14, nrow = 9)) # nrow es numero de años * numero de zonas en la que quiero estimar tendencias (para una especie)
+colnames(m) <- c("sp", "sector", "year", "index", "index_se", "IC95up", "IC95low", "coef_trend", "coef_trend_se", "ptrend", "pgof", "model_fit", "annual_trend", "description")
 
 
 #TRIM ALRUF----
 
-
+for (i in 1:length(sp)){
+  
+  for (j in 1:length(ambit)){
+    
+    # Preparar matriz donde voy a almacenar los datos
+    data <- count_data[which(count_data$sp == sp[i] & count_data$id_ambit == ambit[j] ), ]  # Subset de la especie y el ámbito a analizar
+    m$sp <- sp[i]                                                                           
+    m$sector <- ambit[j]  
+    
+    # Correr modelo 2
+    m2 <- trim(count ~ site + year, data = data, model = 2, serialcor = TRUE, overdisp = TRUE) 
+    
+    # Cálculo del índice 
+    i2 <- index(m2, which="imputed")  
+    m[ ,3:5] <- index(m2, which="imputed")
+    
+    # Cálculo de CI del índice
+     for (k in 1:nrow(m)) {
+       
+       m$IC95up[k] <- m$index[k] + m$index_se[k] * 1.96
+       m$IC95low[k] <- m$index[k] - m$index_se[k] * 1.96
+     }
+    
+    # Cálculo del coeficiente de tendencia (average beta coefficient). Cojo el "multiplicative", que da una mejor interpretación
+    coef <- coefficients(m2)
+    m$coef_trend <- coef$mul
+    m$coef_trend_se <- coef$se_mul
+    
+    
+    # p-value del coeficiente de tendencia (p < 0.05 = tendencia significativa)
+    wald_p <- wald(m2) 
+    m$ptrend <- wald_p$slope$p 
+    
+    # Goodness of fit (p < 0.05 = el modelo no se ajusta )
+    m$pgof <- gof$chi2$p
+    m$model_fit <- ifelse (m$pgof[1] > 0.05, "yes", "no" )
+    
+    # Calcular % de cambio de la población (annual trend)
+    m$annual_trend <- (coef$mul - 1) * 100
+  
+    # Describir la tendencia
+    
+    if()
+    
+    m$description <- ifelse (m$ptrend[1] > 0.05, "yes", "no" )
+      
+      
+  }
+}
 ALRUF<- subset(ambSG, sp == "ALRUF")
 m$sp <- "ALRUF"
 m2 <- trim(count ~ site + year, data = ALRUF, model = 2, serialcor = TRUE, overdisp = TRUE)
@@ -33,18 +80,15 @@ i2 <- index(m2, which="imputed")
 # summarize the model
 summary(m2)
 
-#return time totals
-totals(m1)
-
-wald(m1)
 
 #Retrieve goodness-of-fit
 
-gof(m1)
+gof <- gof(m2)
+gof$chi2$p
 
 #Extract the coefficients
 
-coefficients(m3)
+coefficients(m2)
 index(m3, which="both")
 
 #Plot with overall slope
